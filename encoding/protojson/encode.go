a89133940ba8514e7dfed8fccd3eb8d808732072
Petar Stefek <petar@thefootballcompany.com>
2022-09-01 11:42:13 +0200

    Enable encode 64bit integer as json number - fix for uint

--- encoding/protojson/encode.go
+++ encoding/protojson/encode.go
@@ -273,11 +273,18 @@ func (e encoder) marshalSingular(val protoreflect.Value, fd protoreflect.FieldDe
 	case protoreflect.Uint32Kind, protoreflect.Fixed32Kind:
 		e.WriteUint(val.Uint())
 
-	case protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Uint64Kind,
-		protoreflect.Sfixed64Kind, protoreflect.Fixed64Kind:
+	case protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Sfixed64Kind:
 		// 64-bit integers are written out as JSON string.
 		if e.opts.Encode64BitIntegerAsJsonNumber {
 			e.WriteInt(val.Int())
+		} else {
+			e.WriteString(val.String())
+		}
+
+	case protoreflect.Uint64Kind, protoreflect.Fixed64Kind:
+		// 64-bit integers are written out as JSON string.
+		if e.opts.Encode64BitIntegerAsJsonNumber {
+			e.WriteUint(val.Uint())
 		} else {
 			e.WriteString(val.String())
 		}
